8aad3356e89d6a2cebb30632a417c6a171e332cb
ViktorJp <97465574+ViktorJp@users.noreply.github.com>
2024-03-15 13:07:33 -0400

    Update rtrmon.sh

--- rtrmon.sh
+++ rtrmon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# RTRMON v1.6.5 - Asus-Merlin Router Monitor by Viktor Jaep, 2022-2024
+# RTRMON v1.6.5b1 - Asus-Merlin Router Monitor by Viktor Jaep, 2022-2024
 #
 # RTRMON is a shell script that provides near-realtime stats about your Asus-Merlin firmware router. Instead of having to
 # find this information on various different screens or apps, this tool was built to bring all this info together in one
@@ -17,7 +17,7 @@
 # -------------------------------------------------------------------------------------------------------------------------
 # System Variables (Do not change beyond this point or this may change the programs ability to function correctly)
 # -------------------------------------------------------------------------------------------------------------------------
-Version="1.6.5"
+Version="1.6.5b1"
 Beta=1
 LOGFILE="/jffs/addons/rtrmon.d/rtrmon.log"            # Logfile path/name that captures important date/time events - change
 APPPATH="/jffs/scripts/rtrmon.sh"                     # Path to the location of rtrmon.sh
